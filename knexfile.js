a3388dea4c87a034fea439c32c6a888d06a945c2
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-07 11:37:03 -0300

    feat: using knex and creating api express

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'knex_test',
+      user: 'postgres',
+      password: '1234',
+      host: "localhost",
+      port: 5434
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+
+};
